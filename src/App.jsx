ee436278c58856208bdcdf8f580d67a4c4944d78
andresjaimer-sys <andres.jaimer@uniagustiniana.edu.co>
2026-09-24 14:16:48 +0000

    actualizacion

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Coleccion from "./Componentes/Coleccion"
+import Favoritos from "./Componentes/Favoritos"
+import Inicio from "./Componentes/Inicio"
+import LigaAlemana from "./Componentes/LigaAlemana"
+import Usuario from "./Componentes/Usuario"
+import Info from "./Componentes/Info"
+function App() {
+
+  return (
+    <>
+     <Inicio/> <Coleccion/> <Favoritos/> <Info/> <Usuario/> <LigaAlemana/>
+    </>
+    
+  )
+}
+
+export default App
---
+++ src/Componentes/LigaAlemana/index.jsx
@@ -0,0 +1,12 @@
+import'./style.css'
+
+function LigaAlemana() {
+
+  return (
+    <>
+     Liga Alemana
+    </>
+  )
+}
+
+export default LigaAlemana
